a9ef2c47b2c14f35006b969521f6f874fc2fa7bc
theSoenke <soenke.behrendt@gmail.com>
2023-07-31 11:48:17 +0200

    [TSI-1938] Implement caching (#18)
    * Implement caching and send current_version
    
    * Add memory storage
    
    * Skip multiple deserialization steps

--- src/i18next_phrase_backend.ts
+++ src/i18next_phrase_backend.ts
@@ -10,42 +10,25 @@ export class I18nextPhraseBackend {
   phrase: Phrase | null
   static type: string
 
-  constructor(services: any, options = {distribution: '', secret: '', appVersion: '1.0.0'}) {
+  constructor(services: any, options = { distribution: '', secret: '', appVersion: '1.0.0' }) {
     this.type = 'backend'
     this.options = options
     this.phrase = null
     this.init(services, options)
   }
 
-  init(_services: any, options: {distribution: string, secret: string, appVersion: string}) {
+  init(_services: any, options: { distribution: string, secret: string, appVersion: string }) {
     this.options = options
-    const uuid = this.getUUID()
-    this.phrase = new Phrase(options.distribution, options.secret, options.appVersion, uuid)
+    this.phrase = new Phrase(options.distribution, options.secret, options.appVersion)
   }
 
-  read (language: string, _namespace: string) {
+  read(language: string, _namespace: string) {
     if (this.phrase) {
-      const translation = this.phrase.requestTranslation(language)
-        return new Promise((resolve) => {
-          resolve(translation)
-        })
+      const translation = this.phrase.requestTranslations(language)
+      return new Promise((resolve) => {
+        resolve(translation)
+      })
     }
-  }
-
-  getUUID() {
-    const STORAGE_KEY = "i18nextPhraseBackend.UUID"
-    let uuid = null
-    if (typeof(Storage) !== "undefined") {
-      uuid = localStorage.getItem(STORAGE_KEY)
-      if (!uuid) {
-        uuid = crypto.randomUUID()
-        localStorage.setItem(STORAGE_KEY, uuid)
-      }
-    } else {
-      // Sorry! No Web Storage support..
-      uuid = 'GENERIC_UUID'
-    }
-    return uuid
   }
 }
 
