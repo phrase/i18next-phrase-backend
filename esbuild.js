8c22e05647cd00577619b0ee07a2d26cfc724952
theSoenke <soenke.behrendt@gmail.com>
2023-08-01 14:04:16 +0200

    Use esbuild for bundling (#17)
    * Use esbuild for bundling
    
    * Disable minification
    
    ---------
    
    Co-authored-by: Mladen Jablanovic <mladen.jablanovic@phrase.com>

---
+++ esbuild.js
@@ -0,0 +1,24 @@
+const { build } = require('esbuild');
+const { nodeExternalsPlugin } = require('esbuild-node-externals');
+
+build({
+    entryPoints: ['./src/index.ts'],
+    outfile: 'dist/index.cjs.js',
+    bundle: true,
+    minify: false,
+    treeShaking: true,
+    format: 'cjs',
+    target: ["esnext", "node14"],
+    plugins: [nodeExternalsPlugin()],
+}).catch(() => process.exit(1));
+
+build({
+    entryPoints: ['./src/index.ts'],
+    outfile: 'dist/index.esm.js',
+    bundle: true,
+    minify: false,
+    treeShaking: true,
+    format: 'esm',
+    target: ["esnext", "node14"],
+    plugins: [nodeExternalsPlugin()],
+}).catch(() => process.exit(1));
