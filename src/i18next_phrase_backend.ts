13d4dfef4b4d21be036646440fda20d4c6318328
Sönke Behrendt <soenke.behrendt@gmail.com>
2024-01-23 17:27:31 +0100

    Check randomUUID is available

--- src/i18next_phrase_backend.ts
+++ src/i18next_phrase_backend.ts
@@ -10,32 +10,32 @@ export class I18nextPhraseBackend {
   phrase: Phrase | null
   static type: string
 
-  constructor(services: any, options = {distribution: '', secret: '', appVersion: '1.0.0'}) {
+  constructor(services: any, options = { distribution: '', secret: '', appVersion: '1.0.0' }) {
     this.type = 'backend'
     this.options = options
     this.phrase = null
     this.init(services, options)
   }
 
-  init(_services: any, options: {distribution: string, secret: string, appVersion: string}) {
+  init(_services: any, options: { distribution: string, secret: string, appVersion: string }) {
     this.options = options
     const uuid = this.getUUID()
     this.phrase = new Phrase(options.distribution, options.secret, options.appVersion, uuid)
   }
 
-  read (language: string, _namespace: string) {
+  read(language: string, _namespace: string) {
     if (this.phrase) {
       const translation = this.phrase.requestTranslation(language)
-        return new Promise((resolve) => {
-          resolve(translation)
-        })
+      return new Promise((resolve) => {
+        resolve(translation)
+      })
     }
   }
 
   getUUID() {
     const STORAGE_KEY = "i18nextPhraseBackend.UUID"
     let uuid = null
-    if (typeof(Storage) !== "undefined") {
+    if (typeof (Storage) !== "undefined" || typeof (crypto.randomUUID) !== "undefined") {
       uuid = localStorage.getItem(STORAGE_KEY)
       if (!uuid) {
         uuid = crypto.randomUUID()
